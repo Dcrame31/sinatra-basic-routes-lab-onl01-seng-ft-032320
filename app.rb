f4a42a4153851ef1740f3a1fd00f7a244674c216
Diane Crame <dcrame@icloud.com>
2020-04-27 21:05:35 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Dianne"
+    end
+
+    get '/hometown' do
+        "My hometown is Lemery"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Open"
+    end
 end
